9c0ecb1dc829d1b5e497d8e3f1e4c654e136d4f8
anonimak <anonimak@gmail.com>
2021-06-18 21:44:24 +0700

    penambahan fitur controller

---
+++ controller/controller.php
@@ -0,0 +1,18 @@
+<?php
+
+class Controller
+{
+    public function __construct()
+    {
+    }
+
+    // TODO isi logic index
+    public function index()
+    {
+    }
+
+    // FIXME bug
+    public function update()
+    {
+    }
+}
